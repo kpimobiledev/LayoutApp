def53a4643ee6f535b1ee437bacc9287cd934c51
lidaamber <lidaamber@gmail.com>
2020-03-03 23:09:21 +0200

    basis of constraint layout

--- app/src/main/java/com/lidaamber/layoutapp/MainActivity.kt
+++ app/src/main/java/com/lidaamber/layoutapp/MainActivity.kt
@@ -1,43 +1,14 @@
 package com.lidaamber.layoutapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.lidaamber.layoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    var person = Person("John", "Smith")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.person = person
-
-        binding.changeButton.setOnClickListener {
-            updatePerson()
-
-            Toast.makeText(
-                this,
-                getString(R.string.name_pattern, person.name, person.surname),
-                Toast.LENGTH_LONG
-            ).show()
-
-        }
-    }
-
-    private fun updatePerson() {
-        binding.apply {
-            person?.name = nameEditText.text.toString()
-            person?.surname = surnameEditText.text.toString()
-
-            invalidateAll()
-        }
     }
 }
 
-data class Person(var name: String, var surname: String)
