3bf775dd6aaab2f3f3a6e6ec23f7b074b3cba696
lidaamber <lidaamber@gmail.com>
2020-03-03 22:39:30 +0200

    add data binding

--- app/src/main/java/com/lidaamber/layoutapp/MainActivity.kt
+++ app/src/main/java/com/lidaamber/layoutapp/MainActivity.kt
@@ -3,38 +3,41 @@ package com.lidaamber.layoutapp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.change_button as changeButton
-import kotlinx.android.synthetic.main.activity_main.name_edit_text as nameEditText
-import kotlinx.android.synthetic.main.activity_main.name_text_view as nameTextView
-import kotlinx.android.synthetic.main.activity_main.surname_edit_text as surnameEditText
+import androidx.databinding.DataBindingUtil
+import com.lidaamber.layoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var person = Person("John", "Smith")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val person = Person("John", "Smith")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.person = person
 
-        nameTextView.text = person.name
-
-        nameEditText.setText(person.name)
-        surnameEditText.setText(person.surname)
-
-        changeButton.setOnClickListener {
-            person.name = nameEditText.text.toString()
-            person.surname = surnameEditText.text.toString()
-
-            nameTextView.text = person.name
+        binding.changeButton.setOnClickListener {
+            updatePerson()
 
             Toast.makeText(
                 this,
                 getString(R.string.name_pattern, person.name, person.surname),
                 Toast.LENGTH_LONG
             ).show()
+
         }
     }
 
+    private fun updatePerson() {
+        binding.apply {
+            person?.name = nameEditText.text.toString()
+            person?.surname = surnameEditText.text.toString()
+
+            invalidateAll()
+        }
+    }
 }
 
 data class Person(var name: String, var surname: String)
